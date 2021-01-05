50bf4b10ba33e9d361c01dc7996dfc0d680755b4
Wellington Cunha <wellington@programer.com.br>
2021-01-04 22:57:07 -0300

    my first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+import express from 'express'
+import vhost from 'vhost'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import helmet from 'helmet'
+import https from 'https'
+import http from 'http'
+import fileUpload from 'express-fileupload'
+import mustache from  'mustache'
+import fs from 'fs'
+
+const app = new express()
+
+class SNWS
+{
+    constructor ()
+    {
+
+        this.init()
+    }
+
+    async get_vhosts ()
+    {
+        try
+        {
+            let hosts = await import(`${process.env.PWD}/vhosts.json`)
+
+            if ( hosts.default )
+                hosts = hosts.default
+
+            return hosts
+        }
+        catch ( e )
+        {
+            return []
+        }
+    }
+
+    async init()
+    {
+
+        const hosts = await this.get_vhosts() || []
+
+        try
+        {
+            app.use(bodyParser.json())
+            app.use(bodyParser.text())
+            app.use(bodyParser.urlencoded({extended: false}))
+            app.use(helmet())
+            app.disable(`x-powered-by`)
+            app.set(`trust_proxy`, true);
+            app.use(cors())
+            app.use(fileUpload())
+
+            for ( let i = 0; i < hosts.length; i ++ )
+            {
+                app.use(vhost(hosts[i].host, async (req, res) =>
+                {
+                    global.APP_PATH = hosts[i].APP_PATH || `${process.env.PWD}/www`
+
+                    if ( await fs.existsSync(`${global.APP_PATH}/index.js`) )
+                    {
+                        try
+                        {
+                            let Index = await import(`${global.APP_PATH}/index.js`)
+                            if ( Index.default )
+                                Index = Index.default
+
+                            res.setHeader('Content-Type', 'text/plain')
+                            res.end(await new Index(req, res, app))
+                        }
+                        catch ( e )
+                        {
+                            console.log ( e )
+                        }
+                    }
+                    else if ( await fs.existsSync(`${global.APP_PATH}/index.html`) )
+                    {
+                        res.sendFile(`${global.APP_PATH}/index.html`)
+                    }
+                    else
+                    {
+                        res.setHeader('Content-Type', 'text/plain')
+                        res.end(`No index file!`)
+                    }
+                }))
+            }
+
+            app.listen(80)
+        }
+        catch ( e )
+        {
+            console.log ( e )
+        }
+    }
+}
+
+new SNWS()
